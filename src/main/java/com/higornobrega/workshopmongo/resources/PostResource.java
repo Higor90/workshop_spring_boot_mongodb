4ed17832ccdae5ca75de07df9260af242de55618
Higor Nobrega <nobregahigor@gmail.com>
2023-11-28 17:36:57 -0300

    Consulta simples com query methods.

--- src/main/java/com/higornobrega/workshopmongo/resources/PostResource.java
+++ src/main/java/com/higornobrega/workshopmongo/resources/PostResource.java
@@ -1,15 +1,19 @@
 package com.higornobrega.workshopmongo.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.higornobrega.workshopmongo.domain.Post;
+import com.higornobrega.workshopmongo.resources.util.URL;
 import com.higornobrega.workshopmongo.services.PostService;
 
 @RestController
@@ -26,5 +30,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	
+	@RequestMapping(value ="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findbyTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
